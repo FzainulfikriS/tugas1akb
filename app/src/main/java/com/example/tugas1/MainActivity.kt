51aa156d29518c01183d6611dd70517ce70db33a
pukis <fzainulfikri@email.unikom.ac.id>
2020-04-03 02:38:31 +0700

    baru nyobain tampilan awal background

---
+++ app/src/main/java/com/example/tugas1/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.tugas1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+/* nama  : Faiz Zainulfikri S
+*  nim   : 10117165
+*  kelas : IF5
+* */
