272ebb03b7a954582986c3f4827e12f3ade2705b
pukis <fzainulfikri@email.unikom.ac.id>
2020-04-04 01:30:58 +0700

    beres halaman 1 dan 2

--- app/src/main/java/com/example/tugas1/MainActivity.kt
+++ app/src/main/java/com/example/tugas1/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.tugas1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_awal.setOnClickListener(){
+            val intent = Intent(this, activity2::class.java)
+            startActivity(intent)
+        }
+
     }
 }
+
 /* nama  : Faiz Zainulfikri S
 *  nim   : 10117165
 *  kelas : IF5
