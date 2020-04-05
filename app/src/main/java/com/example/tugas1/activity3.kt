9553e7f9e6680dfce43f43a8a7dda3c1a8eefe4a
pukis <fzainulfikri@email.unikom.ac.id>
2020-04-05 20:39:17 +0700

    membuat tampilan hal 3 dan 4 dan merapihkan tampilan hal 1 2

--- app/src/main/java/com/example/tugas1/activity3.kt
+++ app/src/main/java/com/example/tugas1/activity3.kt
@@ -3,18 +3,21 @@ package com.example.tugas1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_activity2.*
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_activity3.*
 
-class activity2 : AppCompatActivity() {
+class activity3 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_activity2)
+        setContentView(R.layout.activity_activity3)
 
-        btnmasuk.setOnClickListener(){
-            val intent = Intent(this, activity3::class.java)
+        btnselanjutnya.setOnClickListener(){
+            val intent = Intent(this, activity4::class.java)
             startActivity(intent)
         }
+
     }
 }
---
+++ app/src/main/java/com/example/tugas1/activity4.kt
@@ -0,0 +1,12 @@
+package com.example.tugas1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity4 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activity4)
+    }
+}
